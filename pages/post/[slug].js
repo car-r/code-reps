c30738c3e0d7d0438d63c81549bfbde4b6591cac
Carson Richter <carson.ri@protonmail.com>
2022-02-06 20:57:30 -0700

    fallback to true for blog post reload after deloyment

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -46,6 +46,6 @@ export async function getStaticPaths() {
 
     return {
         paths: posts.map(({ node: { slug }}) => ({ params: { slug }})),
-        fallback: false,
+        fallback: true,
     }
 }
