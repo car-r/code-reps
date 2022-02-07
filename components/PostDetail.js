7452beb055a936cc2436f08e43c804d51b742da2
Carson Richter <carson.ri@protonmail.com>
2022-02-06 20:23:39 -0700

    cleanup from raw text and header routing

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -4,44 +4,6 @@ import moment from 'moment'
 import { RichText } from '@graphcms/rich-text-react-renderer';
 
 const PostDetail = ({ post }) => {
-    // const getContentFragment = (index, text, obj, type) => {
-    //     let modifiedText = text;
-    
-    //     if (obj) {
-    //       if (obj.bold) {
-    //         modifiedText = (<b key={index}>{text}</b>);
-    //       }
-    
-    //       if (obj.italic) {
-    //         modifiedText = (<em key={index}>{text}</em>);
-    //       }
-    
-    //       if (obj.underline) {
-    //         modifiedText = (<u key={index}>{text}</u>);
-    //       }
-    //     }
-    
-    //     switch (type) {
-    //       case 'heading-three':
-    //         return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
-    //       case 'paragraph':
-    //         return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
-    //       case 'heading-four':
-    //         return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
-    //       case 'image':
-    //         return (
-    //           <img
-    //             key={index}
-    //             alt={obj.title}
-    //             height={obj.height}
-    //             width={obj.width}
-    //             src={obj.src}
-    //           />
-    //         );
-    //       default:
-    //         return modifiedText;
-    //     }
-    //   };
 
     return (
         <div className='bg-gray-700 bg-opacity-30 rounded-lg lg:p-8 pb-12 mb-8'>
@@ -74,10 +36,7 @@ const PostDetail = ({ post }) => {
                 </div>
                 <h1 className='mb-8 text-2xl md:text-3xl font-semibold'>{post.title}</h1>
                 
-                {/* {post.content.raw.children.map((typeObj, index) => {
-                    const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
-                    return getContentFragment(index, children, typeObj, typeObj.type)
-                })} */}
+
 
                 <RichText 
                   content={post.content.raw}
