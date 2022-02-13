1b226d7ede86f688f42bf11240e73245eba08f37
Carson Richter <carson.ri@protonmail.com>
2022-02-12 21:46:29 -0700

    fetching posts descending via graphCMS

--- services/index.js
+++ services/index.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 export const getPosts = async () => {
     const query = gql`
         query MyQuery {
-            postsConnection {
+            postsConnection(orderBy: createdAt_DESC) {
                 edges {
                     node {
                         author {
@@ -77,7 +77,7 @@ export const getRecentPosts = async () => {
     const query = gql`
         query GetPostDetails() {
             posts (
-                orderBy: createdAt_ASC
+                orderBy: createdAt_DESC
                 last: 3
             ) {
                 title
